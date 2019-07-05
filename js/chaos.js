fe2725204c155fc195519ca647901a33d432c82a
Saggre <sakri.koskimes@joinment.com>
2019-07-05 19:07:29 +0200

    Optimise

--- js/chaos.js
+++ js/chaos.js
@@ -26,7 +26,7 @@ let plotX = 0.0;
 let plotY = 0.0;
 
 // Viewport in world units
-const screenWorldUnits = new THREE.Vector2(10.0, 10.0 * windowH / windowW);
+const screenWorldUnits = new THREE.Vector2(5.0, 5.0 * windowH / windowW);
 
 //Simulation variables
 var t = tStart;
@@ -161,17 +161,24 @@ function applyComputeChaos() {
     var matrixParamsX = new THREE.Matrix3();
     var matrixParamsY = new THREE.Matrix3();
 
-    matrixParamsX.set(
-        params[0], params[1], params[2],
-        params[3], params[4], params[5],
-        params[6], params[7], params[8]
-    );
+    var paramsX = [
+        new THREE.Vector3(params[0], params[1], params[2]),
+        new THREE.Vector3(params[3], params[4], params[5]),
+        new THREE.Vector3(params[6], params[7], params[8])
+    ];
 
-    matrixParamsY.set(
-        params[9], params[10], params[11],
-        params[12], params[13], params[14],
-        params[15], params[16], params[17]
-    );
+    var paramsY = [
+        new THREE.Vector3(params[9], params[10], params[11]),
+        new THREE.Vector3(params[12], params[13], params[14]),
+        new THREE.Vector3(params[15], params[16], params[17])
+    ];
+
+    uniforms.paramsX0.value = paramsX[0];
+    uniforms.paramsX1.value = paramsX[1];
+    uniforms.paramsX2.value = paramsX[2];
+    uniforms.paramsY0.value = paramsY[0];
+    uniforms.paramsY1.value = paramsY[1];
+    uniforms.paramsY2.value = paramsY[2];
 
     uniforms.paramsX.value = matrixParamsX;
     uniforms.paramsY.value = matrixParamsY;
@@ -231,35 +238,31 @@ function init() {
 
     scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        precision: "lowp",
+        depth: false
+    });
+    renderer.debug.checkShaderErrors = debug;
+
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
-
-    var matrixParamsX = new THREE.Matrix3();
-    var matrixParamsY = new THREE.Matrix3();
-
-    matrixParamsX.set(
-        params[0], params[1], params[2],
-        params[3], params[4], params[5],
-        params[6], params[7], params[8]
-    );
-
-    matrixParamsY.set(
-        params[9], params[10], params[11],
-        params[12], params[13], params[14],
-        params[15], params[16], params[17]
-    );
 
     uniforms = {
         iters: {value: iters},
         steps: {value: steps},
         startTime: {value: t},
         deltaTime: {value: 0.01},
-        paramsX: {value: matrixParamsX},
-        paramsY: {value: matrixParamsY},
-        viewport: {value: screenWorldUnits},
+        paramsX: {value: null},
+        paramsY: {value: null},
+        paramsX0: {value: null},
+        paramsX1: {value: null},
+        paramsX2: {value: null},
+        paramsY0: {value: null},
+        paramsY1: {value: null},
+        paramsY2: {value: null},
         colorTexture: {value: createColorTexture()}
     };
 
